ef2ffd730e2d177cd9b55723f7c9f16d252a1a61
Carlos Elizondo <caelra99@gmail.com>
2021-11-02 12:02:16 -0600

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  get :signup,  to: 'users#new'
+  get :login,   to: 'sessions#new'
+
+  resources :sessions, only: %i[create] do
+    delete :destroy, on: :collection
+  end
+
+  resources :users, only: %i[create]
+  resources :dealerships
+  resources :cars
+  resources :dealership_cars
 end
